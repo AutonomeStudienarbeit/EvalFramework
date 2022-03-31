54d03cd20c0265f5d4f2cd6cf1f170719e5cc50e
lkno0705 <lkno0705@gmail.com>
2022-03-31 17:22:32 +0200

    fix imports in fasterRCNN

--- data/models/fasterRCNN/dependencys/engine.py
+++ data/models/fasterRCNN/dependencys/engine.py
@@ -5,9 +5,9 @@ import torch
 
 import torchvision.models.detection.mask_rcnn
 
-from coco_utils import get_coco_api_from_dataset
-from coco_eval import CocoEvaluator
-import utils
+from data.models.fasterRCNN.dependencys.coco_utils import get_coco_api_from_dataset
+from data.models.fasterRCNN.dependencys.coco_eval import CocoEvaluator
+import data.models.fasterRCNN.dependencys.utils as utils
 
 
 def train_one_epoch(model, optimizer, data_loader, device, epoch, print_freq):
