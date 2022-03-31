7e6ad3955899fbba49509b97519a8f6a8d059ef8
lkno0705 <lkno0705@gmail.com>
2022-03-31 17:36:38 +0200

    add prints

--- train_fasterRCNN.py
+++ train_fasterRCNN.py
@@ -2,8 +2,16 @@ from data.datasets import DatasetLoader
 from data.models.fasterRCNN import FasterRCNN
 
 loader = DatasetLoader()
+print("[*] loading Dataset")
 gtsdb_dataset = loader.load_dataset("GTSDB")
+print("[+] Dataset loaded")
+print("[*] inializing Model")
 fasterRCNN = FasterRCNN(dataset=gtsdb_dataset)
+print("[+] Model initialized")
+print("[*] Preparing Dataset for Model")
 fasterRCNN.prepare_dataset(subset_name="train")
-fasterRCNN.set_backbone("resNet50_fpn")
+print("[+] Preparing Dataset for Model finished")
+fasterRCNN.set_backbone("resNet50_fpn", True)
+print("[+] Backbone Set")
+print("[*] starting training")
 fasterRCNN.train(batch_size=2, num_epochs=10)
